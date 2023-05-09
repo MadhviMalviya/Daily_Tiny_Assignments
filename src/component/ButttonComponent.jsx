0cc49b5449cc3539a75fd8b0e5f046259bc464ee
Madhvi Malviya <madhvimalviya1@gmail.com>
2023-05-09 20:15:07 +0530

    button component

---
+++ src/component/ButttonComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ButttonComponent() {
+  return (
+    <div   style={{marginTop:'14rem'}} >
+
+      <h1>Button component</h1>  
+      <button style={{backgroundColor:'blue',color:'white'}} >Click Here!</button>
+    </div>
+  )
+}
+
+export default ButttonComponent
